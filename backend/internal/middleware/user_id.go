df1370a0e823ffccd2a5d36be1cc45f920c7a5e5
yuriechan <28754813+yuriechan@users.noreply.github.com>
2024-03-15 16:30:57 +0900

    add middleware to retrieve uid from request

---
+++ backend/internal/middleware/user_id.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"backend/internal/utils"
+	"fmt"
+	"net/http"
+)
+
+func SetUserID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		uid := r.Header.Get("UserID")
+		if len(uid) == 0 {
+			// For now, when no user ID header is set, treat as unauthorized request
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		fmt.Println("-----------------")
+		fmt.Println(uid)
+		ctx := utils.NewContext(r.Context(), uid)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
